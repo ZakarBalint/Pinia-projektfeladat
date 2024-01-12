3bac1f2c3fed1883b9e410edf3be9a40cdf6d83b
ZakarBalint <szt2zakbal@vasvari.org>
2024-01-12 10:37:21 +0100

    plusz kódok  törölve és adatok bemásolva

--- Pinia-projektfeladat/src/stores/counter.js
+++ Pinia-projektfeladat/src/stores/counter.js
@@ -1,12 +1,32 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
+export const useCounterStore = defineStore('counter', {
+  state: () => ({
+    szinhazak : {
+      neve : 'Vígszínház',
+      eloadasok : [
+        {cim: 'A dzsungel könyve', url : 'https://www.jegy.hu/program/a-dzsungel-konyve-81963'},
+        {cim: 'Barátom, Harvey', url :'https://www.jegy.hu/program/baratom-harvey-131921'},
+        {cim : 'Béranyák', url: 'https://www.jegy.hu/program/beranyak-139745'}
+      ],
+      
+      neve : 'Centrál Színház',
+      eloadasok : [
+        {cim: 'Ma este felnövünk', url : 'https://www.centralszinhaz.hu/eloadasok/ma_este_felnovunk'},
+        {cim: 'Házassági leckék középhaladóknak', url :'https://www.centralszinhaz.hu/eloadasok/hazassagi_leckek_kozephaladoknak'},
+      ],
 
-  return { count, doubleCount, increment }
+      neve : 'Thália Színház',
+      eloadasok : [
+        {cim: 'Legszebb férfikor', url : 'https://thalia.hu/eloadasok/legszebb-ferfikor/'},
+        {cim: 'Bolha a fülbe', url :'https://thalia.hu/eloadasok/bolha-a-fulbe/'},
+        {cim : 'Rettenetes szülők', url: 'https://thalia.hu/eloadasok/jean-cocteau-rettenetes-szulok/'},
+        {cim : 'A nagy kézrablás', url: 'https://thalia.hu/eloadasok/a-nagy-kezrablas-2/'}
+      ]
+    }
+    
+  })
+
+
 })
